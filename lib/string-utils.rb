9adeb787547ec118f543e905e180110f5a227877
Han Loong Liauw <hanloongliauw@gmail.com>
2013-11-01 11:11:32 +1100

    added special case for amp

--- lib/string-utils.rb
+++ lib/string-utils.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 # Silvio Relli's string_utils gem - released under the MIT License
 class String
-  
+
   # Workaround for sanitize method not availabe in controllers, models and libs.
   # Now you can call "mystryng".sanitize everywere.
   def sanitize
@@ -11,21 +11,22 @@ class String
   # Shortcuts
   alias :san :sanitize
   alias :hs :html_safe
-  
+
   # Makes strings be url-friendly
   def urlify
     str = self.downcase.gsub(/ /,'-')
-    str.gsub!(/À|à|á|Â|â|Ã|ã|Ä|ä/i,'a') 
-    str.gsub!(/È|è|É|é|Ê|ê|Ë|ë|&/i,'e')
+    str.gsub!(/À|à|á|Â|â|Ã|ã|Ä|ä/i,'a')
+    str.gsub!(/È|è|É|é|Ê|ê|Ë|ë/i,'e')
     str.gsub!(/Ì|ì|í|Î|î/i,'i')
     str.gsub!(/Ò|ò|ó|Ô|ô|Õ|õ|Ö|ö/i,'o')
     str.gsub!(/Ù|ù|ú|Û|û|Ü|ü/i,'u')
     str.gsub!(/ç/i,'c')
     str.gsub!(/œ/i,'oe')
     str.gsub!(/ß/i,'ss')
+    str.gsub!(/&/i,'and')
     str.gsub!(/[^a-z0-9\-]+/i, '')
     str
   end
-  
+
 end
 
